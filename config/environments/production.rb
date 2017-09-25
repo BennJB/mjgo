cd2668dba4d308a7385fb248cce760130ac7bc47
bennjb <ger_fran@naver.com>
2017-09-25 13:08:23 +0000

    server_change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_cable.allowed_request_origins = ["http://13.124.121.182"]
+  config.action_cable.allowed_request_origins = ["http://13.124.243.82"]
   
   # Code is not reloaded between requests.
   config.cache_classes = true
