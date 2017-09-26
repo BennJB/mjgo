0f2d441e93632f0c965a7dc6e967ae73d464e029
bennjb <ger_fran@naver.com>
2017-09-26 06:32:38 +0000

    tinymce&cook_model edit

--- db/migrate/20170914035252_create_cookreplies.rb
+++ db/migrate/20170914035252_create_cookreplies.rb
@@ -2,7 +2,7 @@ class CreateCookreplies < ActiveRecord::Migration[5.1]
   def change
     create_table :cookreplies do |t|
   
-      t.string :content
+      t.text :content
       t.string :rating
       
       t.integer :cook_id
