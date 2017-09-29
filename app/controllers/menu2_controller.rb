e91dc60509864a3846e071c3135add003dab077b
bennjb <ger_fran@naver.com>
2017-09-29 10:16:07 +0000

    open the menu_position

--- app/controllers/menu2_controller.rb
+++ app/controllers/menu2_controller.rb
@@ -3,7 +3,7 @@ class Menu2Controller < ApplicationController
     layout "menu2"
     
     def menu2
-    @koreacook = Cooklist.find(2).cooks.order('created_at DESC')  
+    @koreacook = Cooklist.find(2).cooks.order('created_at DESC').reverse 
     end
     
 end
--- app/controllers/menu3_controller.rb
+++ app/controllers/menu3_controller.rb
@@ -3,7 +3,7 @@ class Menu3Controller < ApplicationController
     layout "menu3"
     
     def menu3
-    @koreacook = Cooklist.find(3).cooks.order('created_at DESC')  
+    @koreacook = Cooklist.find(3).cooks.order('created_at DESC').reverse
     end
     
 end
